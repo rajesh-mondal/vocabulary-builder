117a083dd1fbd25dbdb1b0250e950c99a3ddd078
Rajesh Mondal <rajesh01749@gmail.com>
2020-10-13 23:30:10 +0600

    user registration done

--- tasks.php
+++ tasks.php
@@ -5,5 +5,12 @@ $connection = mysqli_connect( DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, );
 if ( !$connection ) {
     throw new Exception( "Cannot connect to database" );
 } else {
-    echo "connected";
+    if ( 'register' == $action ) {
+        $username = $_POST['email'] ?? '';
+        $password = $_POST['password'] ?? '';
+        if ( $username && $password ) {
+            $query = "INSERT INTO users(email, password) VALUES ('{$username}','{$password}')";
+            mysqli_query( $connection, $query );
+        }
+    }
 }
